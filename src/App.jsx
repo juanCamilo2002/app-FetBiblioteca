eeefdb0bd06079c5f2a4973fa4e66afdbdf3c4b5
juanCamilo2002 <juan2002djfh@gmail.com>
2024-04-24 16:06:51 -0500

    Diseño de la vista AdminLayout terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import './App.css';
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import ViewBook from './pages/ViewBook/ViewBook';
+import AdminLayout from './pages/admin/Layout/AdminLayout';
+import Dashboard from './pages/admin/dashboard/Dashboard';
+import { AsideProvider } from './context/AsideContext';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path='/viewbook' element={<ViewBook/>}/>
-      </Routes>
-    </BrowserRouter>
+    <AsideProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* user routes */}
+          <Route index path="/" element={<Home />} />
+          <Route path='/viewbook' element={<ViewBook />} />
+
+          {/* admin routes */}
+          <Route path='/admin' element={<AdminLayout />}>
+            <Route path='dashboard' index element={<Dashboard />} />
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AsideProvider>
   );
 }
 
---
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div>
+            DashBoarD        
+        </div>
+    )
+}
+
+export default Dashboard
