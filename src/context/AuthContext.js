a20db7811f93b65993cc60b1fb54f2dee4947caa
juanCamilo2002 <juan2002djfh@gmail.com>
2024-04-28 21:05:13 -0500

    subiendo cambios en el gitignore

---
+++ src/context/AuthContext.js
@@ -0,0 +1,13 @@
+import { createContext, useState, useContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    
+    return (
+        <AuthContext.Provider value={{ user, setUser }}>
+        {children}
+        </AuthContext.Provider>
+    );
+}
