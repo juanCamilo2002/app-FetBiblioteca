ac85b3eff71efdacbf446cfbc0a9c40d54b65ee6
juanCamilo2002 <juan2002djfh@gmail.com>
2024-04-29 14:55:27 -0500

    combinando las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import ViewBook from './pages/ViewBook/ViewBook';
@@ -9,10 +9,9 @@ import { AsideProvider } from './context/AsideContext';
 import AddBook from './pages/admin/AddBook/AddBook';
 import UpdateBook from './pages/admin/UpdateBook/UpdateBook';
 import Book from './pages/admin/books/Book';
-
+import Reservation from './pages/admin/reservations/Reservation';
 
 function App() {
-
   return (
     <AsideProvider>
       <BrowserRouter>
@@ -23,21 +22,19 @@ function App() {
           <Route path='/viewbook' element={<ViewBook />} />
 
           {/* admin routes */}
-
           <Route path='/admin' element={<AdminLayout />}>
             <Route path='dashboard' index element={<Dashboard />} />
-            <Route path='books' index element={<Book />} />
+            <Route path='books' index element={<Book/>} />
             <Route path='reservations' index element={<Reservation/>} />
 
           </Route>
           
         <Route path='/addbook' element={<AddBook/>}/>
         <Route path='/updatebook' element={<UpdateBook/>}/>
-
+        
       </Routes>
       </BrowserRouter>
     </AsideProvider>
-
   );
 }
 
